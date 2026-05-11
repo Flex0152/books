2223d9679413b3cf7899f2acc3c89d5ad006e58b
felix.weidemann <flex89@web.de>
2026-05-11 11:12:49 +0200

    add add_book funktion

--- cli.py
+++ cli.py
@@ -3,29 +3,70 @@ from dependencies import (
     author_service_builder,
     location_service_builder,
 )
-from typer import Typer
+import typer
+from database import session
+
+from rich import print as rprint
+from datetime import datetime
 
 
-app = Typer()
+app = typer.Typer()
 
-books = Typer()
-app.add_typer(books, name="Books")
+books_app = typer.Typer()
+app.add_typer(books_app, name="Books")
 
-genre = Typer()
-app.add_typer(genre, name="Genres")
+genre_app = typer.Typer()
+app.add_typer(genre_app, name="Genres")
 
-location = Typer()
-app.add_typer(location, name="Locations")
+location_app = typer.Typer()
+app.add_typer(location_app, name="Locations")
 
-@books.command("add")
-def add_book(title: str):
-    print(f"Added a new book: {title}")
+@books_app.command("add")
+def add_book(
+    title: str,
+    author: str,
+    genre: str,
+    state: str,
+    location: str,
+    published: str = typer.Option("1900-01-01", help="Format: YYYY-MM-DD")
+) -> None:
+    try:
+        parsed_date = datetime.fromisoformat(published)
+    except ValueError:
+        rprint(":x: invalid date format, please use YYYY-MM-DD")
+        return
+    
+    try:
+        
+        with session() as s:
+            service = book_service_builder(s)
 
-@books.command("delete")
+            book = service.create_book(
+                title=title,
+                author_name=author,
+                genre_name=genre,
+                state_name=state,
+                location_name=location,
+                published=parsed_date
+            )
+
+            s.commit()
+            rprint(f":white_check_mark: The book {book.book_title} has been added successfully.")
+
+    except ValueError as e:
+        rprint(f":x: One of the argument is faulty: {e}")
+
+    except Exception as e:
+        rprint(f":x: An unexpected Error has ocurred: {e}")
+
+
+@books_app.command("delete")
 def delete_book(title: str):
     print(f"Deleted a Book: {title}")
 
-@location.command("add")
+
+
+@location_app.command("add")
 def add_location(name: str):
     print(f"deleted a location: {name}")
 
