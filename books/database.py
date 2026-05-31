0ab8e0913ba22a4421a14001fe561a36a2ae232e
felix <flex89@web.de>
2026-05-31 09:40:15 +0200

    bugfix database file

--- books/database.py
+++ books/database.py
@@ -9,10 +9,10 @@ db_file_path = Path(__file__).parent.parent / "data" / "books.db"
 db_file_path.parent.mkdir(parents=True, exist_ok=True)
 
 if not db_file_path.is_file():
-    engine = sa.create_engine(f"sqlite:///data/{db_file_path}")
+    engine = sa.create_engine(f"sqlite:///{db_file_path}")
     Base.metadata.create_all(engine)
 else:
-    engine = sa.create_engine(f"sqlite:///data/{db_file_path}")
+    engine = sa.create_engine(f"sqlite:///{db_file_path}")
 
 session = sessionmaker(bind=engine)
 
